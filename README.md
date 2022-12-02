8925606d1554a307a974078ef940e653d15e9e7b
eli <ebheravi159@gmail.com>
2022-12-02 05:15:45 -0800

    create project

--- README.md
+++ README.md
@@ -68,3 +68,60 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+
+# Payout Currency Converter 
+​ 
+Your task is to create a simple Payout Currency Converter using Deriv's 
+WebSocket endpoint. The requirements of this task are as the following: 
+​ 
+1. Create a page with an input field, a dropdown and a table. 
+2. Dropdown 
+   - List out all the available payout currencies. The selected item in the 
+dropdown is the `base_currency` 
+3. Input Field 
+   - This sets the value of the `base_currency`. 
+   - Must be valid currency value. 
+4. Table 
+   - List out all of the currencies from `exchange_rates` call. 
+   - 2 Columns: `Currency` and `Value`. 
+      - `Currency` is the currency code (Example: `USD`, `AUD`, etc). 
+      - `Value` is the converted value of the `base_currency` entered in the `Input 
+Field` to the specified `currency`. 
+      - This value must be update on every change on the `Input Field`. 
+5. Formatting 
+   - Round the USD, AUD and GBP exchange rate values to `2` decimal 
+places. Leave the remaining exchange rate values as it is. 
+​ 
+- You need to initiate a connection to deriv websocket, the `ws` endpoint is: 
+`wss://ws.binaryws.com/websockets/v3?app_id=1089`.<br> 
+  Note: `1089` can be used as the `app_id`. Registering a new `app_id` is not 
+needed. 
+​ 
+- An example of how you can initiate a websocket connection to deriv 
+websocket: 
+  Please keep in mind that the `onopen` and `onmessage` event listeners that 
+listed below are just examples of how to send message and get response 
+with deriv ws connection. You need to find the needed parameters from 
+documentation links which provided in the next sections. 
+​ 
+  ```javascript 
+  var ws = new 
+WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089'); 
+   
+  ws.onopen = function(evt) { 
+      ws.send(JSON.stringify({ticks:'R_100'})); 
+  }; 
+   
+  ws.onmessage = function(msg) { 
+     var data = JSON.parse(msg.data); 
+     console.log('ticks update: %o', data); 
+  }; 
+  ``` 
+ 
+​ 
+- List of payout currencies can be obtained using the `payout_currencies` 
+WebSocket API call. Please check the API Playground for `payout_currencies` 
+WebSocket API call - https://api.deriv.com/api-explorer/#payout_currencies 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+var ws = new WebSocket('wss://green.binaryws.com/websockets/v3?app_id=31063');
 
 function App() {
+  const [baseCurrency, setBaseCurrency] = useState({ name: "USD", value: 1 });
+  const [exchangeRates, setExchangeRates] = useState([]);
+  const [payoutCurrencies, setPayoutCurrencies] = useState([]);
+
+  useEffect(() => {
+    ws.onopen = function (evt) {
+      ws.send(JSON.stringify({ "payout_currencies": 1 }));
+      ws.send(JSON.stringify({
+        "exchange_rates": baseCurrency.value || 1,
+        "base_currency": baseCurrency.name || "USD"
+      }));
+    };
+
+    ws.onmessage = function (msg) {
+      var data = JSON.parse(msg.data);
+      if (data.msg_type === "payout_currencies") {
+        setPayoutCurrencies(data.payout_currencies);
+        console.log('ticks update: %o', data.payout_currencies);
+      }
+      if (data.msg_type === "exchange_rates") {
+        console.log('exchange_rates' + JSON.stringify(data.exchange_rates.rates));
+        setExchangeRates([data.exchange_rates.rates]);
+      }
+    };
+  }, []);
+
+
+  useEffect(() => {
+    if (ws.readyState) {
+      ws.send(JSON.stringify({
+        "exchange_rates": +baseCurrency.value,
+        "base_currency": baseCurrency.name
+      }));
+    }
+  }, [baseCurrency]);
+
+
+  const handleOnchange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    setBaseCurrency(baseCurrency => ({ ...baseCurrency, [name]: value }));
+  };
+
+
+  const ListOfpayoutCurrencies = () =>
+    payoutCurrencies.map((payoutCurrency) =>
+      <option key={payoutCurrency} value={payoutCurrency}>
+        {payoutCurrency}
+      </option>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {JSON.stringify(exchangeRates)}
+      <input
+        type="number"
+        value={baseCurrency.value}
+        name="value"
+        onChange={handleOnchange}
+      />
+      <select
+        onChange={handleOnchange}
+        value={baseCurrency.name}
+        name="name"
+      >
+        <ListOfpayoutCurrencies />
+
+      </select>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Currency </th>
+            <th>Value </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* {exchangeRates.map((rate=>{
+               <td>{rate.</td>
+            }))} */}
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
